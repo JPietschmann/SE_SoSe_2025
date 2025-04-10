978671737a28258604c288691040f060a3230c15
JPietschmann <pj7349@mci4me.at>
2025-04-10 17:03:33 +0200

    Aufgabe 6.2

--- my_classes.py
+++ my_classes.py
@@ -1,30 +1,37 @@
-class Subject ():
-    def __init__ (self, first_name: str, last_name: str, age: int, sex: str):
-        self.first_name= first_name
-        self.last_name=  last_name
+class Person ():
+ def __init__(self, first_name: str, last_name: str):
+    self.first_name= first_name
+    self.last_name=  last_name
+    pass
+
+class Subject (Person):
+    def __init__ (self, first_name: str, last_name: str, date_of_birth: str, age: int, sex: str):
+        super.__init__ (first_name, last_name)
+        self.__date_of_birth= date_of_birth
         self.age= age
         self.sex= sex
         pass
 
-    def estimate_max_hr(self):
+    def estimate_max_hr(self): 
         if self.sex == "male":
             max_hr_bpm = 223 - 0.9 * self.age
         elif self.sex == "female":
             max_hr_bpm = 226 - 1.0 * self.age
         else:
             max_hr_bpm = input("Enter maximum heart rate:")
+        return max_hr_bpm
 
-class Supervisor ():
-    def __init__(self, name : str):
-        self.name= name
+class Supervisor (Person):
         pass
+
 class Experiment ():
     def __init__(self, test_id: str, date: str):
         self.test_id= test_id
         self.date= date
-        pass
+
     def add_subject(self, subject):
         self.subject = subject
+
     def add_supervisor(self, supervisor):
         self.supervisor= supervisor
         pass
